a38b67f58cb60a5667fa788ab514c32d3f39bf68
Coded <joshuasokpey@gmail.com>
2026-06-17 13:08:25 +0000

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,32 @@
-public class Algorithms {
+public class Algorithm {
+
+    // Linear Search Method
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found
+            }
+        }
+        return -1; // Target not found
+    }
+
+    // Binary Search Method
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found
+    }
 }
